41623ea26073327202cdbeeaa2c0c9747aab6792
salmakona <salmakona@Televersants-Mac-mini-2.local>
2017-03-21 13:10:06 +0600

    my first commit

---
+++ app/components/item/items.ts
@@ -0,0 +1,9 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+@Component({
+    selector: 'items',
+    templateUrl:'app/components/item/items.html',
+    //styleUrls:['app/components/item/item.css'],
+    encapsulation:ViewEncapsulation.None
+})
+export class ItemComponent {}
+
---
+++ app/components/thef/theftcontrol.ts
@@ -0,0 +1,8 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+@Component({
+    selector: 'theftcontrol',
+    templateUrl:'app/components/thef/theft_control.html',
+    //styleUrls:['app/components/thef/thef.css'],
+   encapsulation:ViewEncapsulation.None
+})
+export class TheftcontrolComponent{}
