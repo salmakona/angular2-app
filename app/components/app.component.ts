1f7548d21cc3358ef2077466e6f2d2f2dee4f7e6
salmakona <salmakona@Televersants-Mac-mini-2.local>
2017-03-28 14:26:12 +0600

    first commit

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,15 +1,19 @@
 import {Component} from '@angular/core';
 import { Router, Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login_register/login';
+import { AuthenticationService } from './login_register/authentication.service';
 @Component({
     selector: 'app',
     templateUrl:'app/components/main.html',
   // styleUrls:['../app/components/style.css'],
-
+    providers:[AuthenticationService]
 })
 export class AppComponent {
+    loggedIn = false;
 
-    constructor(private _router: Router){
+    constructor(private _router: Router,private authenticationService: AuthenticationService,){
         this._router.navigate(["/location"]);
-    }
+        this.loggedIn = this.authenticationService.isLoggedIn();
+        }
 }
 
--- app/components/location/location.ts
+++ app/components/location/location.ts
@@ -3,7 +3,6 @@ import {Component, ViewEncapsulation} from '@angular/core';
 @Component({
     selector: 'location',
     templateUrl:'app/components/location/location.html',
-    //styleUrls:['app/components/location/location.css'],
     encapsulation:ViewEncapsulation.None
 })
 export class LocationComponent{
